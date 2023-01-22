c869edb01ef5122448904acef23cd9d192855450
Lotz0 <juanguastavino97@gmail.com>
2023-01-21 23:14:06 -0300

    2nd

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-let key = "243e022a054d4b42925d8276d67d9759";
-let pais = "ar"
-let url = `https://newsapi.org/v2/top-headlines?country=${pais}&apiKey=${key}`;
-let show_News = document.getElementById("noticias");
+document.addEventListener("DOMContentLoaded", bringProducts)
+const container = document.querySelector("#cards")
 
-fetch(url).then((resp) => resp.json()).then(dato =>{
-    console.log(dato)
-    let noticias = (dato.articles);
-    noticias.map(function(numero){
-        let div = document.createElement("noticias");
-        div.innerHTML= `<br>
-                        <img style="max-width:800px" src=${numero.urlToImage}><br>
-                        <h1>${numero.title}</h1> 
-                        <h2>${numero.description}</h2>
-                        <h3>${numero.url}</h3>
-                        `;
-        show_News.appendChild(div);
-    })
-});
+
+//icono menu
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+}
+
+
+async function bringProducts(){
+    const url = 'https://api.escuelajs.co/api/v1/products'
+
+    try {
+        const result = await fetch(url)
+        const response = await result.json()
+        printProducts(response)
+
+    } catch (error){
+        console.log(error)
+    }
+}
+
+function printProducts(products){
+    console.log(products)
+    products.forEach((prod) => {
+      const{id, title, price, category, description, images} = prod
+        container.innerHTML +=  `
+        <div class="card">
+        <img src=${images} alt=${images}>
+        <h2>${title}</h2>
+        <p>$${price}</p>
+        <button>Add to Cart</button>
+      </div>
+      
+        `;       
+    });
+}
